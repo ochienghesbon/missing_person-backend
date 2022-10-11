dd396eb09d13eeb2c17f0a77370c91cf1bbc0068
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-10-11 21:35:05 +0300

    creates model

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,56 @@
+class PeopleController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :record_unprocessable_entity
+
+  # GET /people
+  def index
+    people = People.all
+    render json: people
+  end
+
+  # GET /people/:id
+  def show
+    people = find_people
+    render json: people
+  end
+
+  # POST /people
+  def create
+    people = People.create(people_params)
+    render json: people, status: :created
+  end
+  
+   # PATCH /people/:id
+   def update
+    people = find_people
+      people.update!(people_params)
+      render json: people
+  end
+
+  # DELETE:id
+def destroy
+    people = find_people
+      people.destroy
+      head :no_content
+  end
+
+
+  private
+
+  def people_params
+    params.permit(:name, :image, :age, :description, :isMissing, :location)
+  end
+
+  def find_people
+    People.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Person not found" }, status: :not_found 
+    end
+
+def record_unprocessable_entity(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+end
---
+++ db/migrate/20221011171437_create_people.rb
@@ -0,0 +1,14 @@
+class CreatePeople < ActiveRecord::Migration[6.1]
+  def change
+    create_table :people do |t|
+      t.string :image
+      t.string :name
+      t.integer :age
+      t.string :location
+      t.string :description
+      t.boolean "isMissing", default: true, null: false
+
+      t.timestamps
+    end
+  end
+end
