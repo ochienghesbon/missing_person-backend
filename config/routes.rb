69239d333ff4716daa35cc01fbbba05c347d9322
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-10-12 22:16:36 +0300

    [update]: add login and delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  
-  resources :users
-  resources :people, only: [:index,:show,:create,:update,:destroy]
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
+  resources :users, only: [:create, :show]
+  resources :people, only: [:index, :show, :create, :update, :destroy]
+  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
