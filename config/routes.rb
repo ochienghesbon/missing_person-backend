78d1eec69069b7112bea84645a905a35bc8ff13c
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-10-14 07:27:47 +0300

    Updates login and signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show]
   resources :people, only: [:index, :show, :create, :update, :destroy]
+
+
   delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   post "/signup", to: "users#create"
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
